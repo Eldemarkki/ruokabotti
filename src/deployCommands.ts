cb23d55783db0a73a4b0ba815b673a744e01f995
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-12-27 21:51:21 +0200

    chore: update deps

--- src/deployCommands.ts
+++ src/deployCommands.ts
@@ -2,6 +2,7 @@ import { REST } from "@discordjs/rest";
 import { RESTPostAPIApplicationCommandsJSONBody, Routes } from "discord-api-types/v9";
 import { data as foodCommand } from "./commands/ruoka";
 import { data as searchCommand } from "./commands/etsi";
+import { data as viikkoCommand } from "./commands/viikko";
 
 const commands: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
@@ -9,6 +10,7 @@ const clientId = String(process.env.CLIENT_ID);
 
 commands.push(foodCommand.toJSON());
 commands.push(searchCommand.toJSON());
+commands.push(viikkoCommand.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN || "");
 
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,12 @@ client.on("interactionCreate", async interaction => {
 	if (interaction.type !== InteractionType.ApplicationCommand) return;
 
 	const { commandName } = interaction;
-	await require(`./commands/${commandName}`).handle(interaction)
+	try {
+		await require(`./commands/${commandName}`).handle(interaction)
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 })
 
 client.login(process.env.DISCORD_TOKEN);
